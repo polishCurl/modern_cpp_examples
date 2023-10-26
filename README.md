2a5be30cabd19b16fa0b85e9f1509dd5cbb49a6b
Krzysztof Koch <loczek3603@gmail.com>
2023-10-26 22:27:05 +0200

    Improve `typeid()` example

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Small examples of modern C++ features
 This project also demonstrates minimal usage of cmake for building c++ projects.
 
 ### List of modern C++ (c++11 or newer) examples
-1. Using `typeid()` for printing type information at runtime
+1. Using `typeid()` for printing type information at runtime and `__cxa_demangle()` for printing demangled names.
--- TypeId/typeid.cpp
+++ TypeId/typeid.cpp
@@ -12,7 +12,7 @@ auto getTypeName(T name) {
       abi::__cxa_demangle(typeid(name).name(), nullptr, 0, &status));
 }
 
-class C {
+class Type {
  public:
   int integer_[20];
 };
@@ -20,5 +20,8 @@ class C {
 int main() {
   std::cout << getTypeName(int{32}) << '\n';
   std::cout << getTypeName(std::vector<std::pair<bool, int>>()) << '\n';
-  ;
+
+  Type t;
+  std::cout << getTypeName(t) << '\n';
+  std::cout << getTypeName(t.integer_) << '\n';
 }
